5547596a7168fc852ec0f6c5f812fe91566df0eb
Matt Oberle <matt.r.oberle@gmail.com>
2017-12-13 13:19:07 -0500

    increases the wait time maximum to 30 seconds

--- mixedcontent/distributed.py
+++ mixedcontent/distributed.py
@@ -33,7 +33,7 @@ def start_selenium_server(timeout=10):
         command_executor='http://127.0.0.1:4444/wd/hub',
         desired_capabilities=DesiredCapabilities.CHROME
     )
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(30)
     return driver
 
 
